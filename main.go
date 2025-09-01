d36a8f83f770ffcbae3033fa832e4680c7ac8157
Stephan Gabert <stepga@nirgendwo.eu>
2025-09-01 14:27:47 +0200

    main.go: brush up code

--- main.go
+++ main.go
@@ -7,27 +7,21 @@ import (
 )
 
 func main() {
-	// Channel for communicating Pin levels
-	levelChan := make(chan godible.PinLevelMessage)
+	pinLevelChannel := make(chan godible.PinLevelMessage)
 
-	p, err := godible.SetupGPIOInput("GPIO24", levelChan)
+	pin, err := godible.SetupGPIOInput("GPIO24", pinLevelChannel)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Main loop, act on level changes
-	for {
-		select {
-		case msg := <-levelChan:
-			if msg.State {
-				log.Printf("Pin %s is High, processing high state tasks", p.Name())
-				// Process high state tasks
-			} else if msg.Reset {
-				log.Printf("Pin %s is Low, resetting to wait for high state", p.Name())
-				// Process resetting logic, if any
-			}
-		default:
-			// Any other ongoing tasks
+	for pinLevelState := range pinLevelChannel {
+		if pinLevelState.State {
+			log.Printf("Pin %s is High", pin.Name())
+		} else if pinLevelState.Reset {
+			log.Printf("Pin %s is Low", pin.Name())
+		} else {
+			log.Fatalf("Unexpected pin state: %+v", pinLevelChannel)
 		}
 	}
 }
