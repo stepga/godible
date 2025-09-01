d0e660a9f0ffefebde4b6eceb656fd4105e9e121
Stephan Gabert <stepga@nirgendwo.eu>
2025-09-01 21:10:50 +0200

    working rising edge detection
    inspired by https://raspi.tv/2014/rpi-gpio-update-and-detecting-both-rising-and-falling-edges

--- main.go
+++ main.go
@@ -1,27 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
-	godible "github.com/stepga/godible/core"
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
+	"periph.io/x/conn/v3/pin"
+	"periph.io/x/host/v3"
 )
 
 func main() {
-	pinLevelChannel := make(chan godible.PinLevelMessage)
-
-	pin, err := godible.SetupGPIOInput("GPIO24", pinLevelChannel)
-	if err != nil {
+	// Load all the drivers:
+	if _, err := host.Init(); err != nil {
 		log.Fatal(err)
 	}
 
-	// Main loop, act on level changes
-	for pinLevelState := range pinLevelChannel {
-		if pinLevelState.State {
-			log.Printf("Pin %s is High", pin.Name())
-		} else if pinLevelState.Reset {
-			log.Printf("Pin %s is Low", pin.Name())
-		} else {
-			log.Fatalf("Unexpected pin state: %+v", pinLevelChannel)
+	// Lookup a pin by its number:
+	p := gpioreg.ByName("GPIO4")
+	if p == nil {
+		log.Fatal("Failed to find GPIO4")
+	}
+
+	//fmt.Printf("%s: %s\n", p, p.Function())
+	pf, ok := p.(pin.PinFunc)
+	if !ok {
+		log.Fatal("pin.PinFunc is not implemented")
+	}
+	fmt.Printf("%s: %s\n", p, pf.Func())
+
+	// Set it as input, with an internal pull down resistor:
+	// XXX // if err := p.In(gpio.PullDown, gpio.BothEdges); err != nil {
+	if err := p.In(gpio.PullDown, gpio.RisingEdge); err != nil {
+		log.Fatal(err)
+	}
+
+	// Wait for edges as detected by the hardware, and print the value read:
+	for {
+		edgeDetected := p.WaitForEdge(0)
+		if !edgeDetected {
+			fmt.Printf("%s -> no edge detected (timeout); this should not happen, as we wait indefinitely\n", time.Now().Format("15:04:05"))
+			continue
 		}
+		fmt.Printf("%s -> %s\n", time.Now().Format("15:04:05"), p.Read())
 	}
 }
