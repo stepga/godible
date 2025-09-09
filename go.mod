bacf600adb4b19bd9e5b1c897d178682cd609fc8
Stephan Gabert <stepga@nirgendwo.eu>
2025-09-09 22:11:43 +0200

    qnd: pcm 16 bit player

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/stepga/godible
 go 1.24.3
 
 require (
+	github.com/anisse/alsa v0.0.0-20190130210209-592d9b53603e
 	periph.io/x/conn/v3 v3.7.2
 	periph.io/x/host/v3 v3.8.5
 )
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
+// test files https://mauvecloud.net/sounds/
+
 import (
+	"io"
 	"log"
 	"os"
 	"time"
 
+	"github.com/anisse/alsa"
 	godible_core "github.com/stepga/godible/core"
 )
 
@@ -24,13 +28,53 @@ func main() {
 			log.Printf("godible: gpio %s may not work, querying its function failed: %s", gpioName, err)
 		}
 
+		// TODO: register dedicated player (play/pause next/previous functions)
+		// TODO: distinguish short vs long button press
 		go godible_core.PinEdgeCallback(pinIO, func() {
 			log.Printf("triggered %s\n", gpioName)
 		})
 	}
+
 	if gpioSetupFailed {
 		os.Exit(1)
 	}
+
+	// TODO: setup some sort of singleton player state instance
+	//   - current song
+	//   - offset/position
+	//   - previous song
+	//   - next song
+	//   - state (pause/play)
+
+	// TODO: web interface
+	//   - upload songs
+	//     - plain mp3/wav files
+	//     - directory with files
+	//   - restructure files/directories
+	//   - spotify (https://github.com/anisse/librespot-golang)
+
+	// TODO: usb webcam module && qr code recognition
+
+	p, err := alsa.NewPlayer(44100, 2, 2, 4096)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer p.Close()
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer f.Close()
+	b, err := io.ReadAll(f)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = p.Write(b)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	for {
 		time.Sleep(5 * time.Second)
 	}
