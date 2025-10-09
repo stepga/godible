46744e2b6c4395e2ca90c79d749da665deafbf2a
Stephan Gabert <stepga@nirgendwo.eu>
2025-10-10 00:16:52 +0200

    commands via channel (2/x): seems to work overall
    TODO: check out whether last seconds/bytes are lost on some songs?

--- internal/player.go
+++ internal/player.go
@@ -9,10 +9,18 @@ import (
 	"github.com/anisse/alsa"
 )
 
+// TODO: instead of hardcoding nonsense strings introduce something like
+//
+//   type command int
+//   const (
+//     TOGGLE command = iota
+//     NEXT
+//     ...
+//   )
+
 const (
 	DATADIR      = "/perm/godible-data/"
 	CMD_TOGGLE   = "TOGGLE" // toggle play or pause
-	CMD_QUIT     = "QUIT"
 	CMD_NEXT     = "NEXT"
 	CMD_PREVIOUS = "PREVIOUS"
 )
@@ -20,14 +28,12 @@ const (
 // TODO: add reading command via unix socket for debugging
 
 type Player struct {
-	current         *list.Element
-	queue           chan *list.Element
+	Command         chan string
 	audioSourceList *list.List
 	cancelfunc      context.CancelFunc
-	Command         chan string
+	current         *list.Element
+	toggleCh        chan bool
 }
-
-// TODO: add sync.Mutex to Player, to synchronize all access/writes to audioSourceList
 
 func NewPlayer() (*Player, error) {
 	audioSourceList := list.New()
@@ -39,24 +45,16 @@ func NewPlayer() (*Player, error) {
 	return &Player{
 		audioSourceList: audioSourceList,
 		Command:         make(chan string),
+		current:         audioSourceList.Front(),
+		toggleCh:        make(chan bool),
 	}, nil
 }
 
-func (player *Player) setCurrent() {
-	if player.current != nil {
-		return
-	}
-
-	select {
-	case as := <-player.queue:
-		player.current = as
-	default:
-		player.current = player.audioSourceList.Front()
-	}
+func (player *Player) isPaused() bool {
+	return player.cancelfunc == nil
 }
 
-func (player *Player) getCurrent() *AudioSource {
-	player.setCurrent()
+func (player *Player) getCurrentAudioSource() *AudioSource {
 	if player.current != nil {
 		as, ok := player.current.Value.(*AudioSource)
 		if ok {
@@ -66,28 +64,35 @@ func (player *Player) getCurrent() *AudioSource {
 	return nil
 }
 
-func doPlay(ctx context.Context, as *AudioSource) {
+func (player *Player) setCurrentsNext() {
+	if player.current != nil {
+		player.current = player.current.Next()
+	}
+	if player.current == nil {
+		player.current = player.audioSourceList.Front()
+	}
+}
+
+func doPlay(ctx context.Context, as *AudioSource) error {
 	slog.Debug("doPlay begin", "path", as.path, "offset", as.offset, "size", as.size)
 
 	// FIXME: first open file, then check samplerate, and then initialize player with 44100 or 48000 Hz
 	alsaplayer, err := alsa.NewPlayer(44100, 2, 2, 4096)
 	if err != nil {
-		slog.Error("alsaplayer could not be initialized", "err", err)
-		return
+		return err
 	}
 	defer alsaplayer.Close()
 
 	file, err := os.Open(as.path)
 	if err != nil {
-		slog.Error("file can not be opened", "path", as.path, "err", err)
-		return
+		return err
 	}
 	defer file.Close()
 
 	if as.offset != 0 {
 		seeked_offset, err := file.Seek(as.offset, 0)
 		if err != nil {
-			slog.Error("failed to continue paused title", "path", as.path, "offset", as.offset, "err", err)
+			return err
 		}
 		slog.Debug("continue paused title", "path", as.path, "offset", as.offset, "seeked_offset", seeked_offset)
 	}
@@ -97,76 +102,76 @@ func doPlay(ctx context.Context, as *AudioSource) {
 	// Therefore, our own WriteCtx is interruptable by introducing a
 	// contexed, oldschool, buffered write.
 	written_bytes, err := WriteCtx(ctx, alsaplayer, file)
-	if err != nil {
-		switch err {
-		case context.Canceled:
-			slog.Debug("cancel playing of current title", "path", as.path, "offset", written_bytes, "size", as.size)
-			as.offset = as.offset + written_bytes
-		default:
-			slog.Error("playing failed", "path", as.path, "err", err)
-		}
-	} else {
+	switch err {
+	case context.Canceled:
+		as.offset = as.offset + written_bytes // FIXME: use WithCancelCause and differ whether Next or whether paused
+	case nil:
 		as.offset = 0
 	}
-	slog.Debug("doPlay done", "path", as.path, "offset", as.offset, "size", as.size)
+	return err
+}
+
+func (player *Player) executeCancel() bool {
+	if player.cancelfunc != nil {
+		player.cancelfunc()
+		player.cancelfunc = nil
+		return true
+	}
+	return false
 }
 
 func (player *Player) Play() {
-	defer func() {
-		player.cancelfunc = nil
-	}()
-
 	for {
-		ctx, cancelfunc := context.WithCancel(context.Background())
-		player.cancelfunc = cancelfunc
+		<-player.toggleCh
 
-		as := player.getCurrent()
-		if as == nil {
-			slog.Error("Play failed: current title can not be detected")
-			return
+		for {
+			as := player.getCurrentAudioSource()
+			if as == nil {
+				slog.Error("could not fetch current AudioSource")
+				os.Exit(1)
+			}
+			ctx, cancelfunc := context.WithCancel(context.Background())
+			player.cancelfunc = cancelfunc
+			err := doPlay(ctx, as)
+			if err == context.Canceled {
+				slog.Debug("interrupt/cancelation", "current", as.path, "offset", as.offset)
+				break
+			}
+			player.setCurrentsNext()
 		}
-		doPlay(ctx, as)
-		if as.offset != 0 {
-			slog.Debug("Play has been interrupted", "current", as.path, "offset", as.offset)
-			// paused via cancelfunc
-			return
-		}
-		player.current = player.current.Next()
 	}
 }
+
 func (player *Player) Toggle() {
-	if player.cancelfunc != nil {
-		player.cancelfunc()
+	if !player.isPaused() {
+		player.executeCancel()
 	} else {
-		player.Play()
+		player.toggleCh <- true
 	}
 }
 
 func (player *Player) Next() {
-	if player.cancelfunc != nil {
-		player.cancelfunc()
-	}
+	player.executeCancel()
 
-	as := player.getCurrent()
+	as := player.getCurrentAudioSource()
 	if as == nil {
-		slog.Error("CMD_NEXT failed: current title can not be detected")
+		slog.Error("current title can not be detected")
 		return
 	}
+	// FIXME: this does not seem to work ... Next-ing songs still results in continuing them
+	// FIXME: use WithCancelCause and differ whether Next or whether paused
 	as.offset = 0
 
-	player.current = player.current.Next()
-	player.Play()
+	player.setCurrentsNext()
+	player.toggleCh <- true
 }
 
 func (player *Player) Previous() {
 	slog.Error("TODO: implement Player.Previous")
 }
 
-func (player *Player) Quit() {
-	slog.Error("TODO: implement Player.Quit")
-}
-
 func (player *Player) Run() {
+	go player.Play()
 	for {
 		// TODO: lock needed (?), and button press cancels immediately the current operation
 		slog.Debug("wait for command")
@@ -174,13 +179,11 @@ func (player *Player) Run() {
 		slog.Debug("received command", "command", command)
 		switch command {
 		case CMD_NEXT:
-			go player.Next()
+			player.Next()
 		case CMD_PREVIOUS:
-			go player.Previous()
-		case CMD_QUIT:
-			go player.Quit()
+			player.Previous()
 		case CMD_TOGGLE:
-			go player.Toggle()
+			player.Toggle()
 		default:
 			slog.Error("unknown command", "command", command)
 		}
