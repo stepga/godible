6b9079bcc4af01104f346a5314cac758f2136580
Stephan Gabert <stepga@nirgendwo.eu>
2025-11-22 22:55:22 +0100

    working simple gorilla websocket echo server
    see https://github.com/gorilla/websocket/blob/main/examples/echo/server.go

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -21,7 +21,6 @@ async function getData() {
 	}
 }
 
-
 document.addEventListener("DOMContentLoaded", function(event) {
 	var slider = document.getElementById("myRange");
 	var output = document.getElementById("demo");
@@ -39,4 +38,55 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		}
 	});
 	newpageButton.addEventListener('click', click_action);
+});
+
+window.addEventListener("load", function(evt) {
+	var output = document.getElementById("output");
+	var input = document.getElementById("input");
+	var ws;
+
+	var print = function(message) {
+		var d = document.createElement("div");
+		d.textContent = message;
+		output.appendChild(d);
+		output.scroll(0, output.scrollHeight);
+	};
+
+	document.getElementById("open").onclick = function(evt) {
+		if (ws) {
+			return false;
+		}
+		ws = new WebSocket("ws://"+window.location.host+"/ws");
+		ws.onopen = function(evt) {
+			print("OPEN");
+		}
+		ws.onclose = function(evt) {
+			print("CLOSE");
+			ws = null;
+		}
+		ws.onmessage = function(evt) {
+			print("RESPONSE: " + evt.data);
+		}
+		ws.onerror = function(evt) {
+			print("ERROR: " + evt.data);
+		}
+		return false;
+	};
+
+	document.getElementById("send").onclick = function(evt) {
+		if (!ws) {
+			return false;
+		}
+		print("SEND: " + input.value);
+		ws.send(input.value);
+		return false;
+	};
+
+	document.getElementById("close").onclick = function(evt) {
+		if (!ws) {
+			return false;
+		}
+		ws.close();
+		return false;
+	};
 });
