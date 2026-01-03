456c63b59f6d9cce740bd8f80384e28c0e92174c
Stephan Gabert <stepga@nirgendwo.eu>
2026-01-03 22:45:05 +0100

    rfid: prevent from crashing due empty tracklist (async CreateTrackList ... grml)

--- src/player.go
+++ src/player.go
@@ -342,8 +342,16 @@ func (player *Player) RfidUidReceiver(uidpass chan string) {
 	trackPathRfidUidMap = make(map[string]string)
 
 	// FIXME: remove this; just for testing
-	track, _ := player.TrackList.Back().Value.(*Track)
-	player.SetRfidTrack("f6084903", track.GetPath())
+	for {
+		if player.TrackList == nil || player.TrackList.Len() == 0 {
+			slog.Debug("XXX: wait for tracklist being built")
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		track, _ := player.TrackList.Front().Value.(*Track)
+		player.SetRfidTrack("f6084903", track.GetPath())
+		break
+	}
 
 	go func() {
 		for {
